3308166a3a86de6d2ea96cf26c968dc57ee59ea5
wcubasdeveloper <stwtml@gmail.com>
2018-12-22 00:09:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,11 +27,8 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    //speech: speech,
-    //displayText: speech,
-    
-    speech: "HOLA TE ESCRIBO DESDE HEROKU",
-    displayText: "HOLA TE ESCRIBO DESDE CHEROKU",
+    speech: speech,
+    displayText: speech,
     source: "webhook-echo-sample"
   });
 });
