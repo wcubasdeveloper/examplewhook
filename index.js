02161507fdd6112a1972af06ce3c1b9d8b850c8d
wcubasdeveloper <stwtml@gmail.com>
2018-12-21 23:50:26 -0500

    quitar restriccion

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
+restService.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 restService.use(
   bodyParser.urlencoded({
     extended: true
