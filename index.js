bd22e96c79ad277f97e013f340c979125f27edb0
wcubasdeveloper <stwtml@gmail.com>
2018-12-22 00:31:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,31 +26,67 @@ restService.post("/echo", function(req, res) {
     req.body.result.parameters.echoText
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
-    return res.json({
-    "queryResult": {
-      "queryText": "hola",
-      "parameters": {
-        "geo-city": ""
-      },
-      "allRequiredParamsPresent": true,
-      "fulfillmentText": "Bienvenido al echo service escrbe algo la csm",
+
+  
+  return res.json(
+
+    {
+      "fulfillmentText": "This is a text response",
       "fulfillmentMessages": [
         {
-          "text": {
-            "text": [
-              "Bienvenido al echo service escrbe algo la csm"
+          "card": {
+            "title": "card title",
+            "subtitle": "card text",
+            "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+            "buttons": [
+              {
+                "text": "button text",
+                "postback": "https://assistant.google.com/"
+              }
             ]
           }
         }
       ],
-      "intent": {
-        "name": "projects/botvendemas/agent/intents/e8b19fd4-6188-40fd-b7bf-0921b6cfce3a",
-        "displayName": "Default Intent"
+      "source": "example.com",
+      "payload": {
+        "google": {
+          "expectUserResponse": true,
+          "richResponse": {
+            "items": [
+              {
+                "simpleResponse": {
+                  "textToSpeech": "this is a simple response"
+                }
+              }
+            ]
+          }
+        },
+        "facebook": {
+          "text": "Hello, Facebook!"
+        },
+        "slack": {
+          "text": "This is a text response for Slack."
+        }
       },
-      "intentDetectionConfidence": 1,
-      "languageCode": "es"
+      "outputContexts": [
+        {
+          "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+          "lifespanCount": 5,
+          "parameters": {
+            "param": "param value"
+          }
+        }
+      ],
+      "followupEventInput": {
+        "name": "event name",
+        "languageCode": "en-US",
+        "parameters": {
+          "param": "param value"
+        }
+      }
     }
-  });
+    
+  );
   
 });
 
